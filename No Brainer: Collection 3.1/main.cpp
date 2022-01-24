83f3584f7fb94810f98b417b906e4e732ccc15ee
Coleton Watt <electricninja102@gmail.com>
2022-01-23 22:01:27 -0700

    Initial Commit

---
+++ No Brainer: Collection 3.1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  No Brainer: Collection 3.1
+//
+//  Created by Coleton Watt on 1/23/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
